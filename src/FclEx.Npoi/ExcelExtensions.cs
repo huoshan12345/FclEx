38aef23fa6435b1ab05acf865f2bc3b766f4cac9
huoshan12345 <89009143@qq.com>
2019-03-19 15:37:47 +0800

    fix a bug

--- src/FclEx.Npoi/ExcelExtensions.cs
+++ src/FclEx.Npoi/ExcelExtensions.cs
@@ -5,11 +5,11 @@ namespace FclEx.Npoi
 {
     public static class ExcelExtensions
     {
-        public static byte[] ToExcelBytes<T>(this ICollection<T> dataSource, IList<IExportColumn<T>> columns)
+        public static byte[] ToExcelBytes<T>(this ICollection<T> dataSource, IList<IExportColumn<T>> columns, string sheetName = "sheet1")
         {
             using (var mem = new MemoryStream())
             {
-                ExcelHelper.ExportExcel(dataSource, OfficeType.Office2007, "sheet1", mem, columns);
+                ExcelHelper.ExportExcel(dataSource, OfficeType.Office2007, sheetName, mem, columns);
                 return mem.ToArray();
             }
         }
