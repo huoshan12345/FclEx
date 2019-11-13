5a52a3277a82b7f546dd9fcd9d85b5051a30bd97
Jeremy Li <jeremyli@seismic.com>
2019-11-13 19:27:42 +0800

    add some extensions

--- src/FclEx/~Extensions/EnumerableExtensions.cs
+++ src/FclEx/~Extensions/EnumerableExtensions.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Dawn;
+using FclEx.Helpers;
 using FclEx.Utils;
 using Microsoft.Collections.Extensions;
 using MoreLinq;
 
 namespace FclEx
 {
-    public static class EnumerableExtensions
+    public static partial class EnumerableExtensions
     {
+
         public static string JoinWith<T>(this IEnumerable<T> strs, string separator)
         {
             return string.Join(separator, strs.Select(m => m.ToString()));
@@ -32,90 +37,19 @@ namespace FclEx
             return condition ? source.Where(predicate) : source;
         }
 
-        public static Task<TResult[]> ForEachAsync<T, TResult>(this IEnumerable<T> sequence,
-            Func<T, Task<TResult>> action)
-        {
-            return Task.WhenAll(sequence.Select(action).ToArray());
-        }
-
-        public static Task ForEachAsync<T>(this IEnumerable<T> sequence, Func<T, Task> action)
-        {
-            return Task.WhenAll(sequence.Select(action).ToArray());
-        }
-
-        public static (T[] True, T[] False) PartitionToArray<T>(this IEnumerable<T> source, Func<T, bool> predicate)
-        {
-            var pair = source.Partition(predicate);
-            return (pair.True.ToArray(), pair.False.ToArray());
-        }
-
-        public static (List<T> True, List<T> False) PartitionToList<T>(this IEnumerable<T> source, Func<T, bool> predicate)
-        {
-            var pair = source.Partition(predicate);
-            return (pair.True.ToList(), pair.False.ToList());
-        }
-
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
         public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
             Func<TSource, TSource, TResult> resultSelector)
         {
             return source.SelectMany(m => source, resultSelector);
         }
 
-        public static IEnumerable<T> Except<T>(
-            this IEnumerable<T> source,
-            T item,
-            IEqualityComparer<T> comparer = null)
+        public static IEnumerable<T> Except<T>(this IEnumerable<T> source,T item,IEqualityComparer<T> comparer = null)
         {
-            comparer = comparer ?? EqualityComparer<T>.Default;
+            comparer ??= EqualityComparer<T>.Default;
             return source.Where(m => !comparer.Equals(m, item));
         }
-
-        public static MultiValueDictionary<TKey, TValue> ToMultiValueDic<T, TKey, TValue>(
-            this IEnumerable<T> enumerable,
-            Func<T, TKey> keySelector,
-            Func<T, IReadOnlyCollection<TValue>> valueSelector)
-        {
-            return new MultiValueDictionary<TKey, TValue>(enumerable.Select(m => KvPair.For(keySelector(m), valueSelector(m))));
-        }
-
-        public static MultiValueDictionary<TKey, TValue> ToMultiValueDic<TKey, TValue>(
-            this IEnumerable<KeyValuePair<TKey, TValue>> enumerable)
-        {
-            var col = enumerable.GroupBy(m => m.Key)
-                .Select(m => KvPair.For(m.Key, (IReadOnlyCollection<TValue>)m.Select(x => x.Value).ToArray()));
-            return new MultiValueDictionary<TKey, TValue>(col);
-        }
-
-        public static OrderedDictionary<TKey, TValue> ToOrderedDic<T, TKey, TValue>(
-            this IEnumerable<T> enumerable,
-            Func<T, TKey> keySelector,
-            Func<T, TValue> valueSelector)
-        {
-            return new OrderedDictionary<TKey, TValue>(enumerable.Select(m => KvPair.For(keySelector(m), valueSelector(m))));
-        }
-
-        public static OrderedDictionary<TKey, TValue> ToOrderedDic<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> enumerable)
-        {
-            return new OrderedDictionary<TKey, TValue>(enumerable);
-        }
-
-        public static (IEnumerable<TProp> True, IEnumerable<TProp> False) Partition<T, TProp>(this IEnumerable<T> enumerable,
-            Func<T, bool> predicate, Func<T, TProp> selector)
-        {
-            var (@true, @false) = enumerable.Partition(predicate);
-            return (@true.Select(selector), @false.Select(selector));
-        }
-
-        public static (T[] True, T[] False) ToArray<T>(this (IEnumerable<T> True, IEnumerable<T> False) enumerable)
-        {
-            return (enumerable.True.ToArray(), enumerable.False.ToArray());
-        }
-
-        public static (List<T> True, List<T> False) ToList<T>(this (IEnumerable<T> True, IEnumerable<T> False) enumerable)
-        {
-            return (enumerable.True.ToList(), enumerable.False.ToList());
-        }
-
+        
         /// <summary>
         /// 获取left和right的差集的第一个元素
         /// </summary>
