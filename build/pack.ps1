8da90abcd1576a39b14dc8559dee85525b788050
huoshan12345 <huoshan12345@hotmail.com>
2022-05-21 19:19:56 +0800

    fix

---
+++ build/pack.ps1
@@ -0,0 +1,34 @@
+$Env:DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER = 0
+$ErrorActionPreference = "Stop"
+
+. "./share.ps1"
+
+# Go back to the pack folder
+Set-Location $packFolder
+Remove-Item *.nupkg
+
+Write-Output "Packing..."
+foreach($path in $projectPaths) { 
+	& dotnet clean $path -v q
+    & dotnet pack $path --nologo -c Release --include-symbols -v q --output $packFolder
+	if ($Lastexitcode -ne 0)	{
+		throw "failed with exit code $LastExitCode"
+	}
+	Write-Output "Packed $($path)"
+}
+
+$PSGallerySourceUri = 'https://www.myget.org/F/huoshan12345/api/v2/package'
+$APIKey = 'fbc0486a-55ff-4760-b246-bef3e0ee952d'
+
+$files = Get-ChildItem ./*.nupkg
+
+Write-Output "Uploading..."
+foreach ($file in $files) {
+	& dotnet nuget push $file -k $APIKey -s $PSGallerySourceUri --timeout 50
+	if ($Lastexitcode -ne 0) {
+		throw "failed with exit code $LastExitCode"
+	}
+}
+
+Write-Output "Finished. Press any key to exit."
+Read-Host
---
+++ build/share.ps1
@@ -0,0 +1,22 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$slnPath = Join-Path $packFolder "../"
+$srcPaths = @(
+([io.path]::combine($slnPath, "src"))
+)
+
+# List of projects
+$projectNames = (
+"FclEx",
+"FclEx.Http",
+"FclEx.Xunit",
+"FclEx.Wmi",
+"FclEx.Serilog"
+)
+
+$projectPaths = new-object 'System.Collections.Generic.List[string]'
+foreach($srcPath in $srcPaths) {
+	$items = Get-ChildItem -Path $srcPath -Include *.csproj -File -Recurse  | 
+	Where-Object { $projectNames -contains $_.BaseName }  | % { $_.FullName }
+	$projectPaths.AddRange( ([string[]]$items) )
+}
