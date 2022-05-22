a97e7fd55c8b081a2a053035709d2d807bb2c3e6
huoshan12345 <huoshan12345@hotmail.com>
2022-05-22 12:48:20 +0800

    x

--- test/FclEx.Http.Test/FclEx/Http/Services/HttpServiceExtensionsTests.cs
+++ test/FclEx.Http.Test/FclEx/Http/Services/HttpServiceExtensionsTests.cs
@@ -14,27 +14,11 @@ namespace FclEx.Http.Services
 {
     public class HttpServiceExtensionsTests
     {
-        public static string[] FileUrls { get; } =
-        {
-            "https://www.baidu.com/",
-            "https://wx4.sinaimg.cn/mw690/006ODuFcly1fv1ey3f7m9j30tn0tnjsi.jpg",
-            "https://www.cnblogs.com/kevinchoi/p/11716636.html#_label_h2_2",
-            "https://www.baidu.com/s?wd=asdf&rsv_spt=1&rsv_iqid=0xb83160ce001946fb&issp=1&f=8&rsv_bp=1",
-            "https://rm.api.weibo.com/2/remind/push_count.json?callback=STK_157208808117322"
-        };
-
-        public static IEnumerable<object[]> TestCasesOfDownload { get; } = FileUrls
-            .Select(m => new Uri(m))
-            .Select(m => (Url: m, FileName: Path.GetFileName(m.LocalPath)))
-            .Select(m => (m.Url, m.FileName, Ext: Path.GetExtension(m.FileName)))
-            .Select(m => new object[] { m.Url, m.FileName, m.Ext, m.FileName.TrimEnd(m.Ext) });
-
         [Theory]
         [InlineData("https://www.baidu.com/", "www_baidu_com.html")]
         [InlineData("https://wx4.sinaimg.cn/mw690/006ODuFcly1fv1ey3f7m9j30tn0tnjsi.jpg", "006ODuFcly1fv1ey3f7m9j30tn0tnjsi.jpg")]
         [InlineData("https://www.cnblogs.com/kevinchoi/p/11716636.html#_label_h2_2", "11716636.html")]
         [InlineData("https://www.baidu.com/s?wd=asdf&rsv_spt=1&rsv_iqid=0xb83160ce001946fb&issp=1&f=8&rsv_bp=1", "s.html")]
-        [InlineData("https://rm.api.weibo.com/2/remind/push_count.json?callback=STK_157208808117322", "push_count.json")]
         public async Task DownloadAsync_Test(string uri, string fileName)
         {
             using var http = new HttpClientService();
