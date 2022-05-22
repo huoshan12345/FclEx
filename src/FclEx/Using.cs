d4a0b82bc395033b5cea2307f8f14f0a3ad97932
huoshan12345 <huoshan12345@hotmail.com>
2022-05-22 19:07:25 +0800

    xx

--- src/FclEx/Using.cs
+++ src/FclEx/Using.cs
@@ -1,5 +1,7 @@
-﻿global using OperateResult = FclEx.Utils.OperateResult<FclEx.Unit>;
+﻿#if !DISABLE_FCLEX_USING
+global using OperateResult = FclEx.Utils.OperateResult<FclEx.Unit>;
 global using FclEx;
+#endif
 
 
 
