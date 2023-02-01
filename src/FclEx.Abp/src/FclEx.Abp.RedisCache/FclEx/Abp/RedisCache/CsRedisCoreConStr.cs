3f2d0df5b1146511e77c8463c14098cd4654fdba
huoshan12345 <huoshan12345@hotmail.com>
2023-02-01 19:26:09 +0800

    x

--- src/FclEx.Abp/src/FclEx.Abp.RedisCache/FclEx/Abp/RedisCache/CsRedisCoreConStr.cs
+++ src/FclEx.Abp/src/FclEx.Abp.RedisCache/FclEx/Abp/RedisCache/CsRedisCoreConStr.cs
@@ -1,4 +1,6 @@
-﻿namespace FclEx.Abp.RedisCache
+﻿using System.Text;
+
+namespace FclEx.Abp.RedisCache
 {
     public class CsRedisCoreConStr
     {
@@ -18,10 +20,39 @@
 
         public override string ToString()
         {
-            return $"{Host}:{Port},password={Password},defaultDatabase={DefaultDatabase}," +
-                   $"connectTimeout={ConnectTimeout},syncTimeout={SyncTimeout},testcluster=false" +
-                   $"poolsize={Poolsize},ssl={Ssl.ToString().ToLower()},writeBuffer={WriteBuffer}," +
-                   $"prefix={Prefix},tryit={TryIt},name={Name},preheat={PreHeat.ToString().ToLower()}";
+            using var sb = new ValueStringBuilder();
+            sb.Append(Host);
+            sb.Append(':');
+            sb.Append(Port.ToString());
+            if (Password.IsValid())
+            {
+                sb.Append(",password=");
+                sb.Append(Password);
+            }
+            sb.Append(",defaultDatabase=");
+            sb.Append(DefaultDatabase.ToString());
+            sb.Append(",connectTimeout=");
+            sb.Append(ConnectTimeout.ToString());
+            sb.Append(",syncTimeout=");
+            sb.Append(SyncTimeout.ToString());
+            sb.Append(",testcluster=");
+            sb.Append(false.ToLower());
+            sb.Append(",poolsize=");
+            sb.Append(Poolsize.ToString());
+            sb.Append(",ssl=");
+            sb.Append(Ssl.ToLower());
+            sb.Append(",writeBuffer=");
+            sb.Append(WriteBuffer.ToString());
+            sb.Append(",prefix=");
+            sb.Append(Prefix);
+            sb.Append(",tryit=");
+            sb.Append(TryIt.ToString());
+            sb.Append(",name=");
+            sb.Append(Name);
+            sb.Append(",preheat=");
+            sb.Append(PreHeat.ToString());
+
+            return sb.ToString();
         }
     }
 }
---
+++ src/FclEx/src/FclEx/FclEx/Extensions/~System/BoolExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FclEx.Extensions;
+
+public static class BoolExtensions
+{
+    public static string ToLower(this bool flag)
+    {
+        return flag ? "true" : "false";
+    }
+}
