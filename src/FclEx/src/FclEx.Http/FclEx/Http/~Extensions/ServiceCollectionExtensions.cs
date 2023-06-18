6df3e6e11760397e629f7915cd37b5dafd187726
huoshan12345 <huoshan12345@hotmail.com>
2023-06-18 13:43:45 +0800

    x

--- src/FclEx/src/FclEx.Http/FclEx/Http/~Extensions/ServiceCollectionExtensions.cs
+++ src/FclEx/src/FclEx.Http/FclEx/Http/~Extensions/ServiceCollectionExtensions.cs
@@ -2,21 +2,6 @@
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddSingletonBy<T, TDependency>(this IServiceCollection services, Func<TDependency, T> func)
-        where TDependency : class
-        where T : class
-    {
-        return services.AddSingleton(s => func(s.GetRequiredService<TDependency>()));
-    }
-
-    public static IServiceCollection AddSingletonBy<T, TDependency1, TDependency2>(this IServiceCollection services, Func<TDependency1, TDependency2, T> func)
-        where TDependency1 : class
-        where TDependency2 : class
-        where T : class
-    {
-        return services.AddSingleton(s => func(s.GetRequiredService<TDependency1>(), s.GetRequiredService<TDependency2>()));
-    }
-
     public static IHttpClientBuilder AddHttpClientWithPolly(this IServiceCollection services, string name, HttpClientOptions? options = null)
     {
         options ??= HttpClientOptions.Default;
