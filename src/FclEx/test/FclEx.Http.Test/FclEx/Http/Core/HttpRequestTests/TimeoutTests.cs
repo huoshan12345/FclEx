a1d941460eb70f008fce6324c379a9fa8c94dd8b
jeremyli <jeremyli@seismic.com>
2023-06-25 20:40:58 +0800

    x

--- src/FclEx/test/FclEx.Http.Test/FclEx/Http/Core/HttpRequestTests/TimeoutTests.cs
+++ src/FclEx/test/FclEx.Http.Test/FclEx/Http/Core/HttpRequestTests/TimeoutTests.cs
@@ -9,11 +9,14 @@ public class TimeoutTests
     [InlineData(3)]
     public async Task ConnectTimeout_Test(int timeoutSeconds)
     {
+        var http = HttpClientService.Create(m => m.RetryCount = 0);
         var timeout = TimeSpan.FromSeconds(timeoutSeconds);
-        var req = HttpRequest.Get("https://httpstat.us/504?sleep=60000")
-            .ReadHeadersTimeout(timeout);
+        var task = HttpRequest.Get("https://httpstat.us/504?sleep=60000")
+            .ReadHeadersTimeout(timeout)
+            .SendAsync(http)
+            .ThrowIfError();
 
-        var (successful, _, exception, elapsed) = await Operate.ExecuteAsync(async () => await req.SendAsync().ThrowIfError());
+        var (successful, _, exception, elapsed) = await Operate.ExecuteAsync(() => task);
         Assert.False(successful);
         Assert.IsType<TaskCanceledException>(exception);
         AssertExt.Equal(timeout, elapsed, TimeSpan.FromSeconds(1));
