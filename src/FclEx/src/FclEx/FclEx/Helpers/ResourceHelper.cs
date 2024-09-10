21baedcdf8c5e3ea36413692905cb1e25f34b7fb
huoshan12345 <huoshan12345@hotmail.com>
2024-09-10 15:52:43 +0800

    fix name

--- src/FclEx/src/FclEx/FclEx/Helpers/ResourceHelper.cs
+++ src/FclEx/src/FclEx/FclEx/Helpers/ResourceHelper.cs
@@ -4,34 +4,34 @@ public static class ResourceHelper
 {
     private static readonly char[] _newLineChars = Environment.NewLine.ToCharArray();
 
-    public static Stream? LoadEmbededResource(Assembly assembly, string name)
+    public static Stream? LoadEmbeddedResource(Assembly assembly, string name)
     {
         var resourceName = assembly.GetManifestResourceNames().FirstOrDefault(p => p.EndsWith(name));
         var stream = resourceName == null ? null : assembly.GetManifestResourceStream(resourceName);
         return stream;
     }
 
-    public static T LoadEmbededResource<T>(Assembly assembly, string name, Func<Stream?, T> func)
+    public static T LoadEmbeddedResource<T>(Assembly assembly, string name, Func<Stream, T> func)
     {
-        using var resource = LoadEmbededResource(assembly, name);
+        using var resource = LoadEmbeddedResource(assembly, name) ?? throw new KeyNotFoundException($"Cannot find embedded resource by name '{name}'");
         return func(resource);
     }
 
-    public static string LoadStringFromEmbededResource(Assembly assembly, string resourceName, Encoding encoding) => LoadEmbededResource(assembly, resourceName, s =>
+    public static string LoadStringFromEmbeddedResource(Assembly assembly, string resourceName, Encoding encoding) => LoadEmbeddedResource(assembly, resourceName, s =>
     {
-        using var sr = new StreamReader(s!, encoding);
+        using var sr = new StreamReader(s, encoding);
         return sr.ReadToEnd();
     });
 
-    public static string LoadStringFromEmbededResource(Assembly assembly, string resourceName) =>
-        LoadStringFromEmbededResource(assembly, resourceName, Encoding.UTF8);
+    public static string LoadStringFromEmbeddedResource(Assembly assembly, string resourceName) =>
+        LoadStringFromEmbeddedResource(assembly, resourceName, Encoding.UTF8);
 
-    public static string[] LoadLinesFromEmbededResource(Assembly assembly, string resourceName, Encoding encoding)
+    public static string[] LoadLinesFromEmbeddedResource(Assembly assembly, string resourceName, Encoding encoding)
     {
-        return LoadStringFromEmbededResource(assembly, resourceName, encoding)
+        return LoadStringFromEmbeddedResource(assembly, resourceName, encoding)
             .Split(_newLineChars, StringSplitOptions.RemoveEmptyEntries);
     }
 
-    public static string[] LoadLinesFromEmbededResource(Assembly assembly, string resourceName) =>
-        LoadLinesFromEmbededResource(assembly, resourceName, Encoding.UTF8);
+    public static string[] LoadLinesFromEmbeddedResource(Assembly assembly, string resourceName) =>
+        LoadLinesFromEmbeddedResource(assembly, resourceName, Encoding.UTF8);
 }
