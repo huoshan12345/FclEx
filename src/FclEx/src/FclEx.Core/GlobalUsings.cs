df28c617b2188641591798d979e1d7f7eada697c
huoshan12345 <huoshan12345@hotmail.com>
2024-10-15 05:01:17 +0800

    add core project

---
+++ src/FclEx/src/FclEx.Core/GlobalUsings.cs
@@ -0,0 +1,26 @@
+// Global using directives
+
+global using System;
+global using System.Collections;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
+global using System.IO;
+global using System.Linq;
+global using System.Linq.Expressions;
+global using System.Net;
+global using System.Net.Http;
+global using System.Net.Sockets;
+global using System.Numerics;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
+global using System.Runtime.ExceptionServices;
+global using System.Runtime.InteropServices;
+global using System.Runtime.Serialization;
+global using System.Text;
+global using System.Text.RegularExpressions;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Web;
