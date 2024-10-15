b19c874d50725926aea698b32694c768b33c209b
huoshan12345 <huoshan12345@hotmail.com>
2024-10-15 15:45:58 +0800

    move files

--- src/FclEx/src/FclEx.Wmi.SourceGenerator/FclEx/Wmi/SourceGenerator/Models/Qualifiers.cs
+++ src/FclEx/src/FclEx.Wmi.SourceGenerator/FclEx/Wmi/SourceGenerator/Models/Qualifiers.cs
@@ -5,6 +5,6 @@ namespace FclEx.Wmi.SourceGenerator.Models;
 
 public class Qualifiers
 {
-    public List<string> Descriptions { get; } = new();
+    public List<string> Descriptions { get; } = [];
     public MultiValueDictionary<string, string> Others { get; } = new();
 }
