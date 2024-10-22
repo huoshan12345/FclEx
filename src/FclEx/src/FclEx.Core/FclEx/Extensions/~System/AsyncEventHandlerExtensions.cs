5d37f8d9c00e69d8d90dbbab5466557821cd2f9b
huoshan12345 <huoshan12345@hotmail.com>
2024-10-23 01:08:53 +0800

    x

--- src/FclEx/src/FclEx.Core/FclEx/Extensions/~System/AsyncEventHandlerExtensions.cs
+++ src/FclEx/src/FclEx.Core/FclEx/Extensions/~System/AsyncEventHandlerExtensions.cs
@@ -5,7 +5,8 @@ public static partial class AsyncEventHandlerExtensions
 {
     public static T[] GetInvocationList<T>(this T @delegate) where T : Delegate
     {
-        return (T[])@delegate.GetInvocationList();
+        // cannot use explicit cast here cause System.InvalidCastException will be raised.
+        return Unsafe.As<T[]>(@delegate.GetInvocationList());
     }
 
     public static Task InvokeAsync<TSender>(this AsyncEventHandler<TSender> handler, TSender sender)
