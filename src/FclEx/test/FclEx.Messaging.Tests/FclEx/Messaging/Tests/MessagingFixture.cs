bd4aa50c57d0785aba8b14986e508e3ad8c05b04
huoshan12345 <huoshan12345@hotmail.com>
2024-12-13 17:07:30 +0800

    fix tests

--- src/FclEx/test/FclEx.Messaging.Tests/FclEx/Messaging/Tests/MessagingFixture.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FclEx.Messaging.Tests;
-
-public class MessagingFixture : GlobalFixture
-{
-    public static ConnectionSettings RmqConnection { get; } = Config.GetSection("Rmq").Get<ConnectionSettings>()!;
-}
