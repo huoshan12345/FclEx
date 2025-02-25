b74e22220d2c2faa9d7ed3257ca4fa18aea1880c
huoshan12345 <huoshan12345@hotmail.com>
2025-02-26 01:58:51 +0800

    add CodeAnalysis

--- src/FclEx.Xunit/Xunit/AssertEx.Bool.cs
+++ src/FclEx.Xunit/Xunit/AssertEx.Bool.cs
@@ -3,13 +3,13 @@
 [SuppressMessage("ReSharper", "ParameterOnlyUsedForPreconditionCheck.Global")]
 partial class AssertEx
 {
-    public static void True(bool condition, Func<string>? userMessage = null)
+    public static void True([DoesNotReturnIf(false)] bool condition, Func<string>? userMessage = null)
     {
         if (!condition)
             Assert.True(condition, userMessage?.Invoke());
     }
 
-    public static void False(bool condition, Func<string>? userMessage = null)
+    public static void False([DoesNotReturnIf(true)] bool condition, Func<string>? userMessage = null)
     {
         if (condition)
             Assert.False(condition, userMessage?.Invoke());
