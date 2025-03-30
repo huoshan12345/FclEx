16b951f199e8cac60a4c07ac81a8c01d8956e05b
huoshan12345 <huoshan12345@hotmail.com>
2025-03-31 07:19:59 +0800

    rename

--- src/FclEx.Core/FclEx/Extensions/~System/~Threading/SemaphoreSlimExtensions.cs
+++ src/FclEx.Core/FclEx/Extensions/~System/~Threading/SemaphoreSlimExtensions.cs
@@ -2,13 +2,13 @@
 
 public static class SemaphoreSlimExtensions
 {
-    public static async Task<bool> WaitAsync(this SemaphoreSlim semaphore, int count,
-        TimeSpan timeout, CancellationToken cancellationToken = default)
+    public static async Task<bool> WaitAsync(this SemaphoreSlim semaphore, int count, TimeSpan timeout, CancellationToken cancellationToken = default)
     {
         for (var i = 0; i < count; ++i)
         {
             var flag = await semaphore.WaitAsync(timeout, cancellationToken);
-            if (!flag) return false; // timeout
+            if (flag == false)
+                return false; // timeout
         }
         return true;
     }
