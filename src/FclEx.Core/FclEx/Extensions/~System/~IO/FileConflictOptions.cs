9ff2b531523d88b26ec4c20d49fb8eaa9d7b9cf5
huoshan12345 <huoshan12345@hotmail.com>
2025-09-25 03:44:21 -0230

    add methods

---
+++ src/FclEx.Core/FclEx/Extensions/~System/~IO/FileConflictOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace FclEx.Extensions;
+
+[Flags]
+public enum FileConflictOptions
+{
+    Cancel = 0,
+    Throw = 1,
+    Overwrite = 1 << 1,
+    Rename = 1 << 2,
+    DeleteOnSame = 1 << 3,
+    /// <summary>
+    /// The value of <see cref="DeleteOnSame"/> | <see cref="Rename"/>
+    /// </summary>
+    Default = DeleteOnSame | Rename,
+}
--- src/FclEx.Core/FclEx/Extensions/~System/~IO/FileInfoExtensions.cs
+++ src/FclEx.Core/FclEx/Extensions/~System/~IO/FileInfoExtensions.cs
@@ -1,23 +1,9 @@
-﻿namespace FclEx.Extensions;
+﻿using static FclEx.Extensions.FileConflictOptions;
+
+namespace FclEx.Extensions;
 
 public static class FileInfoExtensions
 {
-    public static FileInfo Rename(this FileInfo file, string name)
-    {
-        Check.NotNull(file);
-        Check.NotEmpty(name);
-
-        if (file.Name == name)
-            return file;
-
-        var dir = file.DirectoryName;
-        Check.NotNull(dir);
-
-        var newName = Path.Combine(dir, name);
-        file.MoveTo(newName);
-        return new FileInfo(newName);
-    }
-
     public static (string Name, string Ext) GetFileNameAndExtension(this FileInfo file)
     {
         return PathHelper.GetFileNameAndExtension(file.Name);
@@ -55,5 +41,172 @@ public static class FileInfoExtensions
 
         var dest = new FileInfo(Path.Combine(dir.FullName, file.Name));
         return file.CopyToAsync(dest, bufferSize);
+    }
+
+    public static async Task CopyToAsync(this FileInfo file, FileInfo dest, FileConflictOptions options, int bufferSize = 4096)
+    {
+        Check.NotNull(file);
+        Check.NotNull(dest);
+
+        if (file.FullName == dest.FullName)
+            return;
+
+        using var _ = Disposable.Create(() =>
+        {
+            file.Refresh();
+            dest.Refresh();
+        });
+
+        if (dest.Exists == false)
+        {
+            await CopyToAsync(file, dest, bufferSize);
+            return;
+        }
+
+        var option = options & ~DeleteOnSame; // remove flag
+
+        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+        switch (option)
+        {
+            case Cancel: return;
+            case Throw: throw new InvalidOperationException("File already exists: " + dest.FullName);
+            case Overwrite:
+            {
+                await CopyToAsync(file, dest, bufferSize);
+                return;
+            }
+            case FileConflictOptions.Rename:
+            {
+                var newName = FileHelper.GetNextFileName(dest.Name);
+                var newDest = new FileInfo(Path.Combine(dest.DirectoryName!, newName));
+                await file.CopyToAsync(newDest, options, bufferSize);
+                return;
+            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(option), option, null);
+        }
+    }
+
+    public static Task CopyToAsync(this FileInfo file, DirectoryInfo dir, FileConflictOptions options, int bufferSize = 4096)
+    {
+        Check.NotNull(file);
+        Check.NotNull(dir);
+
+        var dest = new FileInfo(Path.Combine(dir.FullName, file.Name));
+        return file.CopyToAsync(dest, options, bufferSize);
+    }
+
+#if NETSTANDARD2_0
+    private static readonly MethodInfo? _methodMoveTo = typeof(FileInfo).GetMethod(
+        name: nameof(FileInfo.MoveTo),
+        bindingAttr: BindingFlags.Public | BindingFlags.Instance,
+        binder: null,
+        types: [typeof(string), typeof(bool)], 
+        modifiers: null);
+
+    public static void MoveTo(this FileInfo file, string destFileName, bool overwrite)
+    {
+        if (_methodMoveTo is {} method)
+        {
+            method.Invoke(file, [destFileName, overwrite]);
+        }
+        else if (overwrite == false)
+        {
+            file.MoveTo(destFileName);
+        }
+        else
+        {
+            if (File.Exists(destFileName))
+                File.Delete(destFileName);
+
+            file.MoveTo(destFileName);
+        }
+    }
+#endif
+
+    public static void MoveTo(this FileInfo file, FileInfo dest, FileConflictOptions options = Default)
+    {
+        Check.NotNull(file);
+        Check.NotNull(dest);
+        Check.EqualTo(file.Exists, true);
+
+        if (file.FullName == dest.FullName)
+            return;
+
+        using var _ = Disposable.Create(() =>
+        {
+            file.Refresh();
+            dest.Refresh();
+        });
+
+        if (dest.Exists == false)
+        {
+            file.MoveTo(dest.FullName, false);
+            return;
+        }
+
+        if (options.HasFlag(DeleteOnSame))
+        {
+            if (dest.Exists && FileHelper.AreFilesEqual(file, dest))
+            {
+                file.Delete();
+                return;
+            }
+        }
+
+        var option = options & ~DeleteOnSame; // remove flag
+
+        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+        switch (option)
+        {
+            case Cancel: return;
+            case Throw: throw new InvalidOperationException("File already exists: " + dest.FullName);
+            case Overwrite:
+            {
+                file.MoveTo(dest.FullName, true);
+                return;
+            }
+            case FileConflictOptions.Rename:
+            {
+                var newName = FileHelper.GetNextFileName(dest.Name);
+                var newDest = new FileInfo(Path.Combine(dest.DirectoryName!, newName));
+                file.MoveTo(newDest, options);
+                return;
+            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(option), option, null);
+        }
+    }
+
+    public static void MoveTo(this FileInfo file, DirectoryInfo dir, FileConflictOptions options = Default)
+    {
+        Check.NotNull(file);
+        Check.NotNull(dir);
+
+        var dest = new FileInfo(Path.Combine(dir.FullName, file.Name));
+        file.MoveTo(dest, options);
+    }
+
+    public static void MoveTo(this FileInfo file, DirectoryInfo? dir, string name, bool appendExt = false, FileConflictOptions options = Default)
+    {
+        Check.NotNull(file);
+        Check.NotEmpty(name);
+
+        var dirPath = (dir?.FullName ?? file.DirectoryName)
+                      ?? throw new InvalidOperationException($"Both {nameof(dir)} and {nameof(file)}.{nameof(file.DirectoryName)} are null.");
+
+        if (appendExt)
+        {
+            var ext = Path.GetExtension(file.Name);
+            name += ext;
+        }
+
+        var dest = new FileInfo(Path.Combine(dirPath, name));
+        file.MoveTo(dest, options);
+    }
+
+    public static void Rename(this FileInfo file, string name, bool appendExt = false, FileConflictOptions options = Default)
+    {
+        file.MoveTo(null, name, appendExt, options);
     }
 }
