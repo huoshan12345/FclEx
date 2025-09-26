bb33b97a564549228b53b6d5bef64992dc2ffc48
huoshan12345 <huoshan12345@hotmail.com>
2025-09-26 12:29:19 -0230

    add method

---
+++ src/FclEx.Core/FclEx/Extensions/~System/~Text/~Json/JsonNodeExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace FclEx.Extensions;
+
+public static class JsonNodeExtensions
+{
+    /// <summary>
+    /// Retrieves the child <see cref="JsonNode"/> at the specified <paramref name="key"/> if it exists
+    /// and is of type <typeparamref name="TNode"/>.  
+    /// If not, creates a new instance of <typeparamref name="TNode"/> using the provided <paramref name="creator"/>,
+    /// assigns it to the <paramref name="key"/> in the parent <paramref name="node"/>, and returns it.
+    /// </summary>
+    /// <typeparam name="TNode">The expected type of the child node.</typeparam>
+    /// <param name="node">The parent <see cref="JsonNode"/> to search in.</param>
+    /// <param name="key">The property name to retrieve or assign.</param>
+    /// <param name="creator">A factory function used to create a new <typeparamref name="TNode"/> if none exists.</param>
+    /// <returns>
+    /// The existing <typeparamref name="TNode"/> at the given <paramref name="key"/>, 
+    /// or the newly created one if none was found.
+    /// </returns>
+    public static TNode GetOrAdd<TNode>(this JsonNode node, string key, Func<TNode> creator) where TNode : JsonNode
+    {
+        if (node[key] is TNode existing)
+            return existing;
+
+        var newNode = creator();
+        node[key] = newNode;
+        return newNode;
+    }
+}
