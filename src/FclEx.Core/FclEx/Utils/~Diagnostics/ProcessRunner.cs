6a331f042d4039af5e711d8969a369545b99aa6f
huoshan12345 <huoshan12345@hotmail.com>
2025-09-28 02:09:51 -0230

    add method

--- src/FclEx.Core/FclEx/Utils/~Diagnostics/ProcessRunner.cs
+++ src/FclEx.Core/FclEx/Utils/~Diagnostics/ProcessRunner.cs
@@ -10,6 +10,8 @@ public class ProcessRunner : IDisposable
 {
     public Process Process { get; }
 
+    public ProcessRunner(string fileName) : this(new ProcessRunnerOptions { FileName = fileName }) { }
+
     public ProcessRunner(ProcessRunnerOptions options)
     {
         Process = new Process
@@ -17,17 +19,39 @@ public class ProcessRunner : IDisposable
             StartInfo = new ProcessStartInfo
             {
                 FileName = options.FileName,
-                RedirectStandardError = true,
                 RedirectStandardOutput = true,
+                // RedirectStandardError = true,
+                RedirectStandardInput = true,
                 CreateNoWindow = true,
                 UseShellExecute = false,
                 WorkingDirectory = options.WorkingDirectory,
+#if NET5_0_OR_GREATER
+                StandardInputEncoding = Encoding.UTF8,
+#endif
+                //StandardErrorEncoding = Encoding.UTF8,
                 StandardOutputEncoding = Encoding.UTF8,
-                StandardErrorEncoding = Encoding.UTF8,
             },
             EnableRaisingEvents = true,
         };
         Process.Start();
+    }
+
+    public async Task<string> ExecuteAsync(string commandText, CancellationToken cancellationToken = default)
+    {
+        await Process.StandardInput.WriteLineAsync(commandText);
+        await Process.StandardInput.FlushAsync(cancellationToken);
+
+        var builder = new StringBuilder();
+        while (true)
+        {
+            var line = await Process.StandardOutput.ReadLineAsync(cancellationToken);
+            if (line == null) 
+                break;
+
+            builder.AppendLine(line);
+        }
+
+        return builder.ToString();
     }
 
     public void Dispose()
---
+++ test/FclEx.Core.Tests/FclEx/Utils/Diagnostics/ProcessRunnerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace FclEx.Utils.Diagnostics;
+
+public class ProcessRunnerTests
+{
+    [LocalOnlyFact]
+    public async Task GetChildItem_Test()
+    {
+        using var runner = new ProcessRunner("pwsh");
+        var result = await runner.ExecuteAsync("Get-Location");
+        Assert.Contains("Directory:", result);
+
+        await runner.ExecuteAsync("exit");
+        await runner.Process.WaitForExitAsync();
+    }
+}
