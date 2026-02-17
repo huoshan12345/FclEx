9459d180d996c3ad771cb4a3c1b64799d9d1f08a
huoshan12345 <huoshan12345@hotmail.com>
2026-02-17 00:41:42 -0330

    fix methods

---
+++ test/FclEx.Core.Tests/FclEx/Extensions/StringBuilderExtensions/AppendWrappedTests.Extensions.cs
@@ -0,0 +1,238 @@
+﻿namespace FclEx.Extensions.StringBuilderExtensions;
+
+partial class AppendWrappedTests
+{
+    // ------------------------------------------------------------
+    // Single quoted
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendSingleQuoted_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendSingleQuoted("abc");
+        Assert.Equal("'abc'", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendSingleQuoted_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendSingleQuoted((string?)null);
+        Assert.Equal("''", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendSingleQuoted_Action()
+    {
+        var sb = new StringBuilder();
+        var invoked = false;
+
+        sb.AppendSingleQuoted(b =>
+        {
+            invoked = true;
+            b.Append("abc");
+        });
+
+        Assert.True(invoked);
+        Assert.Equal("'abc'", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Double quoted
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendDoubleQuoted_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendDoubleQuoted("abc");
+        Assert.Equal("\"abc\"", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendDoubleQuoted_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendDoubleQuoted((string?)null);
+        Assert.Equal("\"\"", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendDoubleQuoted_Action()
+    {
+        var sb = new StringBuilder();
+        sb.AppendDoubleQuoted(b => b.Append("abc"));
+        Assert.Equal("\"abc\"", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Parenthesized
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendParenthesized_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendParenthesized("abc");
+        Assert.Equal("(abc)", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendParenthesized_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendParenthesized((string?)null);
+        Assert.Equal("()", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendParenthesized_Action()
+    {
+        var sb = new StringBuilder();
+        sb.AppendParenthesized(b => b.Append("abc"));
+        Assert.Equal("(abc)", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Square bracketed
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendSquareBracketed_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendSquareBracketed("abc");
+        Assert.Equal("[abc]", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendSquareBracketed_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendSquareBracketed((string?)null);
+        Assert.Equal("[]", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendSquareBracketed_Action()
+    {
+        var sb = new StringBuilder();
+        sb.AppendSquareBracketed(b => b.Append("abc"));
+        Assert.Equal("[abc]", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Curly braced
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendCurlyBraced_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendCurlyBraced("abc");
+        Assert.Equal("{abc}", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendCurlyBraced_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendCurlyBraced((string?)null);
+        Assert.Equal("{}", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendCurlyBraced_Action()
+    {
+        var sb = new StringBuilder();
+        sb.AppendCurlyBraced(b => b.Append("abc"));
+        Assert.Equal("{abc}", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Angle bracketed
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendAngleBracketed_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendAngleBracketed("abc");
+        Assert.Equal("<abc>", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendAngleBracketed_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendAngleBracketed((string?)null);
+        Assert.Equal("<>", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendAngleBracketed_Action()
+    {
+        var sb = new StringBuilder();
+        sb.AppendAngleBracketed(b => b.Append("abc"));
+        Assert.Equal("<abc>", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Backticked
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendBackticked_Value()
+    {
+        var sb = new StringBuilder();
+        sb.AppendBackticked("abc");
+        Assert.Equal("`abc`", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendBackticked_NullValue()
+    {
+        var sb = new StringBuilder();
+        sb.AppendBackticked((string?)null);
+        Assert.Equal("``", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendBackticked_Action()
+    {
+        var sb = new StringBuilder();
+        sb.AppendBackticked(b => b.Append("abc"));
+        Assert.Equal("`abc`", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Fluent behavior
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void Methods_AreFluent()
+    {
+        var sb = new StringBuilder();
+
+        var result = sb.AppendSingleQuoted("x");
+
+        Assert.Same(sb, result);
+    }
+
+    [Fact]
+    public void Methods_ComposeCorrectly()
+    {
+        var sb = new StringBuilder();
+
+        sb.Append("A")
+          .AppendSingleQuoted("B")
+          .AppendParenthesized("C")
+          .AppendSquareBracketed(b => b.Append("D"))
+          .AppendCurlyBraced("E")
+          .AppendAngleBracketed("F")
+          .AppendBackticked("G");
+
+        Assert.Equal("A'B'(C)[D]{E}<F>`G`", sb.ToString());
+    }
+}
---
+++ test/FclEx.Core.Tests/FclEx/Extensions/StringBuilderExtensions/AppendWrappedTests.cs
@@ -0,0 +1,205 @@
+﻿namespace FclEx.Extensions.StringBuilderExtensions;
+
+public partial class AppendWrappedTests
+{
+    // ------------------------------------------------------------
+    // Action<StringBuilder> overload (string)
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendWrapped_String_Action_WithExplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped("(", b => b.Append("abc"), ")");
+
+        Assert.Equal("(abc)", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_String_Action_WithImplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped("\"", b => b.Append("abc"));
+
+        Assert.Equal("\"abc\"", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_String_Action_DelegateIsInvoked()
+    {
+        var sb = new StringBuilder();
+        var invoked = false;
+
+        sb.AppendWrapped("[", b =>
+        {
+            invoked = true;
+            b.Append('x');
+        }, "]");
+
+        Assert.True(invoked);
+        Assert.Equal("[x]", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_String_Action_NullAction()
+    {
+        var sb = new StringBuilder();
+        var ex = Assert.Throws<ArgumentNullException>(() => sb.AppendWrapped("[", (Action<StringBuilder>?)null!, "]"));
+        Assert.Contains("'appendContent'", ex.Message);
+    }
+
+    [Fact]
+    public void AppendWrapped_String_Action_EmptyOpenOrClose()
+    {
+        // the open is empty
+        {
+            var sb = new StringBuilder();
+            var ex = Assert.Throws<ArgumentException>(() => sb.AppendWrapped("", m => { }, ")"));
+            Assert.Contains("'open'", ex.Message);
+        }
+
+        // the close is empty
+        {
+            var sb = new StringBuilder();
+            var ex = Assert.Throws<ArgumentException>(() => sb.AppendWrapped("(", m => { }, ""));
+            Assert.Contains("'close'", ex.Message);
+        }
+
+        // the open is empty and close is null (which defaults to open)
+        {
+            var sb = new StringBuilder();
+            var ex = Assert.Throws<ArgumentException>(() => sb.AppendWrapped("", m => { }));
+            Assert.Contains("'open'", ex.Message);
+        }
+
+        // the open is empty and close is empty
+        {
+            var sb = new StringBuilder();
+            var ex = Assert.Throws<ArgumentException>(() => sb.AppendWrapped("", m => { }, ""));
+            Assert.Contains("'open'", ex.Message);
+        }
+    }
+
+    // ------------------------------------------------------------
+    // Action<StringBuilder> overload (char)
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendWrapped_Char_Action_WithExplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped('(', b => b.Append("abc"), ')');
+
+        Assert.Equal("(abc)", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_Char_Action_WithImplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped('"', b => b.Append("abc"));
+
+        Assert.Equal("\"abc\"", sb.ToString());
+    }
+    
+    [Fact]
+    public void AppendWrapped_Char_Action_NullAction()
+    {
+        var sb = new StringBuilder();
+        var ex = Assert.Throws<ArgumentNullException>(() => sb.AppendWrapped('"', (Action<StringBuilder>?)null!, '"'));
+        Assert.Contains("'appendContent'", ex.Message);
+    }
+
+    // ------------------------------------------------------------
+    // string value overload (string)
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendWrapped_String_Value_WithExplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped("(", "abc", ")");
+
+        Assert.Equal("(abc)", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_String_Value_WithImplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped("\"", "abc");
+
+        Assert.Equal("\"abc\"", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_String_Value_NullValue()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped("(", (string?)null, ")");
+
+        Assert.Equal("()", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // string value overload (char)
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendWrapped_Char_Value_WithExplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped('(', "abc", ')');
+
+        Assert.Equal("(abc)", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendWrapped_Char_Value_WithImplicitClose()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendWrapped('"', "abc");
+
+        Assert.Equal("\"abc\"", sb.ToString());
+    }
+
+    // ------------------------------------------------------------
+    // Fluent chaining
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendWrapped_IsFluent()
+    {
+        var sb = new StringBuilder();
+
+        var result = sb.AppendWrapped("(", "abc", ")");
+
+        Assert.Same(sb, result);
+    }
+
+    // ------------------------------------------------------------
+    // Multi-call composition
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void AppendWrapped_MultipleCalls_ComposeCorrectly()
+    {
+        var sb = new StringBuilder();
+
+        sb.Append("X")
+            .AppendWrapped("(", "a", ")")
+            .Append("Y")
+            .AppendWrapped("[", b => b.Append("b"), "]");
+
+        Assert.Equal("X(a)Y[b]", sb.ToString());
+    }
+}
