100e6ba23a47e67f977e75c02cd74b5a24c1fd7c
huoshan12345 <huoshan12345@hotmail.com>
2026-05-28 23:38:38 -0230

    fix UserClientHttpAction

--- src/FclEx.Http/FclEx/Web/~Actions/UserClientHttpAction.cs
+++ src/FclEx.Http/FclEx/Web/~Actions/UserClientHttpAction.cs
@@ -11,7 +11,7 @@ public abstract class UserClientHttpAction<TClient, TAccount, T>(TClient client)
     public virtual IHttpService HttpService { get; } = client.HttpService;
 
     public abstract OperationResult<T> GetResult(HttpResponse response);
-    public Task<HttpResponse> GetResponseAsync(HttpRequest request, CancellationToken token = default)
+    public virtual Task<HttpResponse> GetResponseAsync(HttpRequest request, CancellationToken token = default)
         => DefaultHttpAction.GetResponseAsync(this, request, token);
     public virtual Task<OperationResult<HttpResponse>> HandleResponseAsync(HttpResponse response)
         => DefaultHttpAction.HandleResponseAsync(this, response);
