b31b370a34dfe3a7bdb34ca98e7edcc25af015ff
huoshan12345 <huoshan12345@hotmail.com>
2026-07-27 20:23:05 -0230

    add method to ModelBuilderExtensions

--- src/FclEx.EfCore/FclEx/EfCore/~Extensions/ModelBuilderExtensions.cs
+++ src/FclEx.EfCore/FclEx/EfCore/~Extensions/ModelBuilderExtensions.cs
@@ -7,19 +7,43 @@ public static class ModelBuilderExtensions
         return modelBuilder.Entity<TEntity>().ExcludeFromMigrations(tableName);
     }
 
+    private static LambdaExpression GetLambdaExpression<T>(Expression<Func<T, bool>> filter, Type targetType)
+    {
+        var parameter = Expression.Parameter(targetType);
+        var body = ReplacingExpressionVisitor.Replace(filter.Parameters.First(), parameter, filter.Body);
+        return Expression.Lambda(body, parameter);
+    }
+
     public static ModelBuilder HasQueryFilter<T>(this ModelBuilder modelBuilder, IMutableEntityType type, Expression<Func<T, bool>>? filter)
     {
-        if (filter is null)
+        if (filter is null || type.ClrType.IsAssignableTo(typeof(T)) == false)
             return modelBuilder;
 
-        if (type.ClrType.IsAssignableTo(typeof(T)) == false)
-            return modelBuilder;
-
-        var builder = modelBuilder.Entity(type.Name);
-        var parameter = Expression.Parameter(type.ClrType);
-        var body = ReplacingExpressionVisitor.Replace(filter.Parameters.First(), parameter, filter.Body);
-        var lambda = Expression.Lambda(body, parameter);
+        var builder = modelBuilder.Entity(type.ClrType);
+        var lambda = GetLambdaExpression(filter, type.ClrType);
         builder.HasQueryFilter(lambda);
         return modelBuilder;
     }
+
+#if NET10_0_OR_GREATER
+    public static ModelBuilder HasQueryFilter<T>(this ModelBuilder modelBuilder, IMutableEntityType type, string? filterKey, Expression<Func<T, bool>>? filter)
+    {
+        if (filter is null || type.ClrType.IsAssignableTo(typeof(T)) == false)
+            return modelBuilder;
+
+        var builder = modelBuilder.Entity(type.ClrType);
+        var lambda = GetLambdaExpression(filter, type.ClrType);
+
+        if (filterKey is null)
+        {
+            builder.HasQueryFilter(lambda);
+        }
+        else
+        {
+            builder.HasQueryFilter(filterKey, lambda);
+        }
+
+        return modelBuilder;
+    }    
+#endif
 }
