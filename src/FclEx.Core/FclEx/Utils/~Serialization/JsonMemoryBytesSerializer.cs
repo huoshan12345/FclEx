f703de71f7247a66295a14e05d2c66b27ca33734
huoshan12345 <huoshan12345@hotmail.com>
2026-08-15 14:14:34 -0230

    x

--- src/FclEx.Core/FclEx/Utils/~Serialization/JsonMemoryBytesSerializer.cs
+++ src/FclEx.Core/FclEx/Utils/~Serialization/JsonMemoryBytesSerializer.cs
@@ -1,14 +1,15 @@
 namespace FclEx.Utils;
 
-public class JsonMemoryBytesSerializer(IStringSerializer StringSerializer) : IMemoryBytesSerializer
+public class JsonMemoryBytesSerializer(IStringSerializer stringSerializer) : IMemoryBytesSerializer
 {
     public static readonly JsonMemoryBytesSerializer Instance = new(StringAsRawSerializer.Instance);
 
-    public ReadOnlyMemory<byte> Serialize(object? obj) => StringSerializer.Serialize(obj).ToBytes(Encoding.UTF8);
+    public ReadOnlyMemory<byte> Serialize(object? obj)
+        => stringSerializer.Serialize(obj).ToBytes(Encoding.UTF8);
 
     public object? Deserialize(ReadOnlyMemory<byte> data, Type type)
     {
         var str = data.Span.GetString();
-        return StringSerializer.Deserialize(str, type);
+        return stringSerializer.Deserialize(str, type);
     }
 }
