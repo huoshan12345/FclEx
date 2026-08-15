b31a3cae61181d9cc1dafb47e721ab41a16ae356
huoshan12345 <huoshan12345@hotmail.com>
2026-08-15 14:11:05 -0230

    x

---
+++ src/FclEx.Core/FclEx/Utils/~Text/~Json/JsonValidator.cs
@@ -0,0 +1,235 @@
+﻿namespace FclEx.Utils;
+
+public static class JsonValidator
+{
+    public static bool IsValid(string s)
+    {
+        var i = 0;
+        SkipWhitespace(s, ref i);
+
+        var ok = ParseValue(s, ref i);
+
+        SkipWhitespace(s, ref i);
+        return ok && i == s.Length;
+    }
+
+    static bool ParseValue(string s, ref int i)
+    {
+        SkipWhitespace(s, ref i);
+
+        if (i >= s.Length)
+            return false;
+
+        var c = s[i];
+
+        switch (c)
+        {
+            case '{': return ParseObject(s, ref i);
+            case '[': return ParseArray(s, ref i);
+            case '"': return ParseString(s, ref i);
+        }
+
+        if (c == '-' || char.IsDigit(c))
+            return ParseNumber(s, ref i);
+
+        if (Match(s, ref i, "true"))
+            return true;
+
+        if (Match(s, ref i, "false"))
+            return true;
+
+        if (Match(s, ref i, "null"))
+            return true;
+
+        return false;
+    }
+
+    static bool ParseObject(string s, ref int i)
+    {
+        if (!Consume(s, ref i, '{'))
+            return false;
+
+        SkipWhitespace(s, ref i);
+
+        if (Consume(s, ref i, '}'))
+            return true;
+
+        while (true)
+        {
+            SkipWhitespace(s, ref i);
+
+            if (!ParseString(s, ref i))
+                return false;
+
+            SkipWhitespace(s, ref i);
+
+            if (!Consume(s, ref i, ':'))
+                return false;
+
+            if (!ParseValue(s, ref i))
+                return false;
+
+            SkipWhitespace(s, ref i);
+
+            if (Consume(s, ref i, '}'))
+                return true;
+
+            if (!Consume(s, ref i, ','))
+                return false;
+        }
+    }
+
+    static bool ParseArray(string s, ref int i)
+    {
+        if (!Consume(s, ref i, '['))
+            return false;
+
+        SkipWhitespace(s, ref i);
+
+        if (Consume(s, ref i, ']'))
+            return true;
+
+        while (true)
+        {
+            if (!ParseValue(s, ref i))
+                return false;
+
+            SkipWhitespace(s, ref i);
+
+            if (Consume(s, ref i, ']'))
+                return true;
+
+            if (!Consume(s, ref i, ','))
+                return false;
+        }
+    }
+
+    static bool ParseString(string s, ref int i)
+    {
+        if (!Consume(s, ref i, '"'))
+            return false;
+
+        while (i < s.Length)
+        {
+            var c = s[i++];
+
+            if (c == '"')
+                return true;
+
+            if (c == '\\')
+            {
+                if (i >= s.Length)
+                    return false;
+
+                var esc = s[i++];
+
+                // Check for valid escape sequences
+                if ("\"\\/bfnrt".Contains(esc))
+                    continue;
+
+                if (esc == 'u')
+                {
+                    for (var k = 0; k < 4; k++)
+                    {
+                        if (i >= s.Length || !IsHex(s[i++]))
+                            return false;
+                    }
+                    continue;
+                }
+
+                return false;
+            }
+
+            if (c < 0x20)
+                return false;
+        }
+
+        return false;
+    }
+
+    static bool ParseNumber(string s, ref int i)
+    {
+        var start = i;
+
+        if (i < s.Length && s[i] == '-') i++;
+
+        if (i >= s.Length)
+            return false;
+
+        if (s[i] == '0')
+        {
+            i++;
+        }
+        else if (char.IsDigit(s[i]))
+        {
+            while (i < s.Length && char.IsDigit(s[i])) i++;
+        }
+        else return false;
+
+        if (i < s.Length && s[i] == '.')
+        {
+            i++;
+
+            if (i >= s.Length || !char.IsDigit(s[i]))
+                return false;
+
+            while (i < s.Length && char.IsDigit(s[i]))
+                i++;
+        }
+
+        if (i < s.Length && (s[i] == 'e' || s[i] == 'E'))
+        {
+            i++;
+            if (i < s.Length && (s[i] == '+' || s[i] == '-'))
+                i++;
+
+            if (i >= s.Length || !char.IsDigit(s[i]))
+                return false;
+
+            while (i < s.Length && char.IsDigit(s[i]))
+                i++;
+        }
+
+        return i > start;
+    }
+
+    static void SkipWhitespace(string s, ref int i)
+    {
+        while (i < s.Length && char.IsWhiteSpace(s[i]))
+            i++;
+    }
+
+    static bool Consume(string s, ref int i, char c)
+    {
+        if (i < s.Length && s[i] == c)
+        {
+            i++;
+            return true;
+        }
+        return false;
+    }
+
+    static bool Match(string s, ref int i, string keyword)
+    {
+        var len = keyword.Length;
+
+        if (i + len > s.Length)
+            return false;
+
+        for (var k = 0; k < len; k++)
+        {
+            if (s[i + k] != keyword[k])
+                return false;
+        }
+
+        i += len;
+        return true;
+    }
+
+    static bool IsHex(char c)
+    {
+        return c is >= '0' and <= '9'
+            or >= 'a' and <= 'f'
+            or >= 'A' and <= 'F';
+    }
+}
