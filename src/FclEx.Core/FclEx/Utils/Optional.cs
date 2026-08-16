eda3aa8b8c6cffef450d912c91da436f1225360f
huoshan12345 <huoshan12345@hotmail.com>
2026-08-16 03:24:32 -0230

    x

--- src/FclEx.Core/FclEx/Utils/Optional.cs
+++ src/FclEx.Core/FclEx/Utils/Optional.cs
@@ -19,7 +19,7 @@ public static class Optional
     /// <typeparam name="T">The type of the value that will be wrapped in the optional.</typeparam>
     /// <param name="value">The value to be wrapped in the optional.</param>
     /// <returns>An <see cref="Optional{T}"/> containing the specified value.</returns>
-    public static Optional<T> Some<T>(T value) => value;
+    public static Optional<T> Some<T>(T value) => Check.NotNull(value);
 }
 
 /// <summary>
