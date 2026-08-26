bff0f1ead81864c57cbb2cbe8d41b329efc5ead5
huoshan12345 <huoshan12345@hotmail.com>
2026-08-25 21:52:45 -0230

    refine files

--- src/FclEx.Core/FclEx/Extensions/~System/~StringExtensions/StringExtensions.State.cs
+++ src/FclEx.Core/FclEx/Extensions/~System/~StringExtensions/StringExtensions.State.cs
@@ -3,13 +3,20 @@ namespace FclEx.Extensions;
 partial class StringExtensions
 {
     [MethodImpl(AggressiveInlining)]
-    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? str) => string.IsNullOrEmpty(str);
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? str)
+        => string.IsNullOrEmpty(str);
 
     [MethodImpl(AggressiveInlining)]
-    public static bool IsNotEmpty([NotNullWhen(true)] this string? str) => str.IsNullOrEmpty() == false;
+    public static bool IsNotEmpty([NotNullWhen(true)] this string? str)
+        => string.IsNullOrEmpty(str) == false;
 
     [MethodImpl(AggressiveInlining)]
-    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? str) => string.IsNullOrWhiteSpace(str);
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? str)
+        => string.IsNullOrWhiteSpace(str);
+
+    [MethodImpl(AggressiveInlining)]
+    public static bool IsNotBlank([NotNullWhen(true)] this string? str)
+        => string.IsNullOrWhiteSpace(str) == false;
 
     public static string IfEmpty(this string? str, string defaultValue)
     {
