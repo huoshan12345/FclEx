1e36c3aae71a33f6d73fc6a26513d029644bf78a
huoshan12345 <huoshan12345@hotmail.com>
2026-09-22 00:12:27 -0230

    remove method

--- src/FclEx.Http/FclEx/Http/~Core/HttpResponseExtensions.cs
+++ src/FclEx.Http/FclEx/Http/~Core/HttpResponseExtensions.cs
@@ -201,13 +201,6 @@ public static class HttpResponseExtensions
         return response.TryCreateRedirectAction(httpService, r => url);
     }
 
-    public static IAction<HttpResponse> ToAction(this HttpResponse response)
-    {
-        return response.IsError
-            ? ErrorAction.Create<HttpResponse>(response.Exception, response.Elapsed)
-            : SuccessAction.Create(response, response.Elapsed);
-    }
-
     public static OperationResult<HttpResponse> ToOperationResult(this HttpResponse response)
     {
         return response.IsError
